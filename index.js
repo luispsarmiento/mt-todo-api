f6be98d98506478aae3cb3a2a47fb402fe4c8cfa
José Luis <j.louis1493@gmail.com>
2024-03-22 03:38:16 +0000

    Added: security packages

--- index.js
+++ index.js
@@ -2,9 +2,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const routerApi = require('./routes');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const xss = require('xss-clean');
+const cors = require('cors');
 const {errorHandler, boomErrorHandler} = require('./middleware/error');
 
 app.use(express.json());
+
+// Sanitize data
+
+// Security headers
+app.use(helmet());
+
+// To prevent XSS attacks
+app.use(xss());
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes,
+  max: 500
+});
+
+app.use(limiter);
+
+// Prevent http param pllution
+app.use(hpp());
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('MT To-Do will be amazing!');
